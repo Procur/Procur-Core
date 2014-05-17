c6dd9348ef7d98941d659f32f68d73fb39ac29b2
Will Ricketts <wricketts@procur.com>
2014-05-16 22:59:24 -0400

    added simple authentication system

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -7,15 +7,11 @@
  * @docs        :: http://sailsjs.org/#!documentation/policies
  *
  */
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
 
-  // User is allowed, proceed to the next policy, 
-  // or if this is the last policy, the controller
-  if (req.session.authenticated) {
-    return next();
-  }
-
+  // User is allowed, proceed to controller
+  var is_auth = req.isAuthenticated()
+  if (is_auth) return next();
   // User is not allowed
-  // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You are not permitted to perform this action.');
+  else return res.redirect("/");
 };
--- config/routes.js
+++ config/routes.js
@@ -30,11 +30,26 @@ module.exports.routes = {
 
   // By default, your root route (aka home page) points to a view
   // located at `views/home/index.ejs`
-  // 
+  //
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
-    view: 'home/index'
+    controller: 'PostController',
+    action: 'index'
+  },
+
+  'get /login': {
+    controller: 'AuthController',
+    action: 'login'
+  },
+  'post /login': {
+    controller: 'AuthController',
+    action: 'process'
+  },
+  '/logout': {
+    controller: 'AuthController',
+    action: 'logout'
   }
+
 
   /*
   // But what if you want your home page to display
@@ -68,7 +83,7 @@ module.exports.routes = {
   // If you want to set up a route only for one in particular
   // (GET, POST, PUT, DELETE, etc.), just specify the verb before the path.
   // For example, if you have a `UserController` with a `signup` action,
-  // and somewhere else, you're serving a signup form looks like: 
+  // and somewhere else, you're serving a signup form looks like:
   //
   //		<form action="/signup">
   //			<input name="username" type="text"/>
@@ -97,7 +112,7 @@ module.exports.routes = {
 
 
 
-/** 
+/**
  * (3) Action blueprints
  * These routes can be disabled by setting (in `config/controllers.js`):
  * `module.exports.controllers.blueprints.actions = false`
@@ -163,4 +178,3 @@ module.exports.routes = {
  * Finally, if nothing else matched, the default 404 handler is triggered.
  * See `config/404.js` to adjust your app's 404 logic.
  */
- 
