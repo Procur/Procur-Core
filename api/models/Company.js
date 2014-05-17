a64ed4da9a1a31f6007c1a00e9248cab7c47f194
Will Ricketts <wricketts@procur.com>
2014-05-17 17:13:20 -0400

    Fleshed out Company model

--- api/models/Company.js
+++ api/models/Company.js
@@ -9,11 +9,74 @@
 module.exports = {
 
   attributes: {
-  	
-  	/* e.g.
-  	nickname: 'string'
-  	*/
-    
+
+  	name: {
+      type: 'string',
+      required: true
+    },
+    phoneNumber: {
+      type: 'string',
+      required: true
+    },
+    phoneExtension: {
+      type: 'string'
+    },
+    faxNumber: {
+      type: 'string'
+    },
+    email: {
+      type: 'string',
+      required: true
+    },
+    website: {
+      type: 'string'
+    },
+    physicalAddress1: {
+      type: 'string',
+      required: 'true'
+    },
+    physicalAddress2: {
+      type: 'string'
+    },
+    country: {
+      type: 'string',
+      required: 'true'
+    },
+    province: {
+      type: 'string',
+      required: true
+    },
+    postalCode: {
+      type: 'string',
+      required: true
+    },
+    hqAddress1: {
+      type: 'string'
+    },
+    hqAddress2: {
+      type: 'string'
+    },
+    hqCountry: {
+      type: 'string'
+    },
+    hqProvince: {
+      type: 'string'
+    },
+    hqPostalCode: {
+      type: 'string'
+    },
+    companyType: {
+      type: 'string',
+      required: true
+    },
+    industry: {
+      type: 'string'
+    },
+    employeeCount: {
+      type: 'integer',
+      required: true
+    }
+
   }
 
 };
