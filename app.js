1761aeca7c547ddd80127051882e672978e44a68
Will Ricketts <wricketts@procur.com>
2014-05-21 09:44:37 -0400

    Fixing

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
 // Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv,
-  if(process.env.NODETIME_ACCOUNT_KEY) {
-    require('nodetime').profile({
-      accountKey: process.env.NODETIME_ACCOUNT_KEY,
-      appName: 'My Application Name' // optional
-    });
-  }
-);
+require('sails').lift(require('optimist').argv);
