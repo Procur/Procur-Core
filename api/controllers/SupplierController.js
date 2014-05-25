a4a4bc7490827a3507c3b5bfa550f3ec2e84ae67
Will <will@willricketts.com>
2014-05-25 10:08:10 -0400

    Added action skeletons in SupplierController

--- api/controllers/SupplierController.js
+++ api/controllers/SupplierController.js
@@ -17,8 +17,20 @@
 
 module.exports = {
 
+  index: function(req, res){
+
+  },
+
   create: function(req, res){
-    
+
+  },
+
+  update: function(req, res){
+
+  },
+
+  destroy: function(req, res){
+
   }
 
 };
