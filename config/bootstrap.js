cb9a5faa9a95b545e799e97f0d5b4539685679b3
Will Ricketts <will@willricketts.com>
2014-05-28 09:39:40 -0400

    added newrelic support for monitoring

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,8 +9,9 @@
  */
 
 module.exports.bootstrap = function (cb) {
+  sails.newrelic = require('newrelic');
 
-  // It's very important to trigger this callack method when you are finished 
+  // It's very important to trigger this callack method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
 };
---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['procur-core'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '13dca9072265bcbf41b040820eaff71164093298',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
