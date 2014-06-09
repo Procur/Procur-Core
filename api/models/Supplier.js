5081ac32994eb8e729b3aa99ecbadd68df0e9ae8
Adam Morgan <atom-morgan@gmail.com>
2014-06-09 14:12:32 -0400

    Update Supplier and Company MVC

--- api/models/Supplier.js
+++ api/models/Supplier.js
@@ -32,7 +32,7 @@
     language: {
       type: 'array'
     },
-    annualProductionVolume: {
+    annualSalesValue: {
       type: 'string'
     },
     primaryProductSpeciality: {
@@ -62,6 +62,122 @@
     privateLabeler: {
       type: 'boolean'
     },
+    facebook: {
+      type: 'string'
+    },
+    twitter: {
+      type: 'string'
+    },
+    pinterest: {
+      type: 'string'
+    },
+    tumblr: {
+      type: 'string'
+    },
+    linkedin: {
+      type: 'string'
+    },
+    instagram: {
+      type: 'string'
+    },
+    google: {
+      type: 'string'
+    },
+    relevantLinkTitle: {
+      type: 'array'
+    },
+    relevantLinkUrl: {
+      type: 'array'
+    },
+    dunsNumber: {
+      type: 'string'
+    },
+    contactName: {
+      type: 'string'
+    },
+    contactPosition: {
+      type: 'string'
+    },
+    contactEmail: {
+      type: 'string'
+    },
+    companyDescription: {
+      type: 'string'
+    },
+    downloadTitle: {
+      type: 'array'
+    },
+    downloadFile: {
+      type: 'array'
+    },
+    photo: {
+      type: 'array'
+    },
+    environmentalSustainability: {
+      type: 'string'
+    },
+    qualitySourcing: {
+      type: 'string'
+    },
+    workplaceSafety: {
+      type: 'string'
+    },
+    laborEducationTraining: {
+      type: 'string'
+    },
+    reinvestment: {
+      type: 'string'
+    },
+    companyCapabilities: {
+      type: 'string'
+    },
+    asia: {
+      type: 'string'
+    },
+    africa: {
+      type: 'integer'
+    },
+    northAmerica: {
+      type: 'integer'
+    },
+    centralAmerica: {
+      type: 'integer'
+    },
+    carribean: {
+      type: 'integer'
+    },
+    southAmerica: {
+      type: 'integer'
+    },
+    westEurope: {
+      type: 'integer'
+    },
+    eastEurope: {
+      type: 'integer'
+    },
+    oceania: {
+      type: 'integer'
+    },
+    australia: {
+      type: 'integer'
+    },
+    totalFactorySize: {
+      type: 'string'
+    },
+    totalQCStaff: {
+      type: 'string'
+    },
+    coreProductDetails: {
+      type: 'string'
+    },
+    gsaApprovedSupplier: {
+      type: 'string'
+    },
+    cageCode: {
+      type: 'string'
+    }
+
+
 
     //UTILITY
 
