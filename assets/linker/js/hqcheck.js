381f0ddf5e75adb314c8da26d4387ca3915afc10
Cael Olsen <colsen@procur.com>
2014-06-11 16:26:44 -0400

    company and hq fields now copy over on click

--- assets/linker/js/hqcheck.js
+++ assets/linker/js/hqcheck.js
@@ -1,10 +1,23 @@
+// It all begins with a click
 $( ".hqbox" ).click(function() {
 
+// If the hq box is checked...
 if($(".hqbox").is(':checked')) {
-	$(".hqaddress").prop('disabled', true);
+
+// Disable hq field entry...
+	$(".hqfield").prop('disabled', true);
+
+// And copy over the values from the company fields
+	$(".hqaddress1").val($(".address1").val());
+	$(".hqaddress2").val($(".address2").val());
+	$(".hqcompanyCountry").val($(".companyCountry").val());
+	$(".hqcompanyProvince").val($(".companyProvince").val());
+	$(".hqPostalCode").val($(".companyPostalCode").val());
 } 
+
+// Otherwise, forget about it...
 else {
-	$(".hqaddress").prop('disabled', false);
+	$(".hqfield").prop('disabled', false);
 }
 
 });
