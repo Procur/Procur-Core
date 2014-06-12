6066841a86e48a05eb0e46e88e80d94ca6c37188
Adam Morgan <atom-morgan@gmail.com>
2014-06-12 14:25:25 -0400

    Add image upload functionality to registration wizards

---
+++ config/imageUploadHelper.js
@@ -0,0 +1,49 @@
+var cloudinary = require('cloudinary');
+var fs = require('fs');
+
+module.exports.imageUploadHelper = {
+  
+  getFileSize: function(file) {
+    var stats = fs.statSync(file);
+    var fileSize = stats['size'];
+    if (fileSize > 0){
+      return true;
+    }
+    else {
+      return false;
+    }
+  },
+
+  uploadSupplierImage: function(req, res, userObj, image) {
+    cloudinary.uploader.upload(image, function(result) {
+      Supplier.update(userObj, { logoUrl: result.url }, function(err, supplier) {
+        if (err) { return res.redirect('/') };
+      });
+    },
+    {
+      format: 'jpg',
+      width: 150,
+      height: 150,
+      crop: 'thumb',
+      gravity: 'face',
+      radisu: 'max'
+    });
+  },
+
+  uploadBuyerImage: function(req, res, userObj, image) {
+    cloudinary.uploader.upload(image, function(result) {
+      Buyer.update(userObj, { logoUrl: result.url }, function(err, buyer) {
+        if (err) { return res.redirect('/') };
+      });
+    },
+    {
+      format: 'jpg',
+      width: 150,
+      height: 150,
+      crop: 'thumb',
+      gravity: 'face',
+      radisu: 'max'
+    });
+  }
+
+}
