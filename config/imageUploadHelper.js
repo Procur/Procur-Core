4f658a5ec17761b6880a754f35e74c70f194ae4d
Adam Morgan <atom-morgan@gmail.com>
2014-06-12 16:20:04 -0400

    Front-end code cleanup :)

--- config/imageUploadHelper.js
+++ config/imageUploadHelper.js
@@ -26,7 +26,7 @@ module.exports.imageUploadHelper = {
       height: 150,
       crop: 'thumb',
       gravity: 'face',
-      radisu: 'max'
+      radius: 'max'
     });
   },
 
@@ -42,7 +42,7 @@ module.exports.imageUploadHelper = {
       height: 150,
       crop: 'thumb',
       gravity: 'face',
-      radisu: 'max'
+      radius: 'max'
     });
   }
 
