bdd734584d9f291406056e667e5a7420f161f88e
Trey Schneider <tschneider@procur.com>
2014-06-19 14:53:43 -0400

    welcome page copy country state fully functional

--- assets/linker/js/hqcheck.js
+++ assets/linker/js/hqcheck.js
@@ -4,25 +4,32 @@ $( ".hqbox" ).click(function() {
 // If the hq box is checked...
 if($(".hqbox").is(':checked')) {
 
+
+
+
 // Disable hq field entry...
-	// $(".hqfield").prop('disabled', true);
 
 
-  alert($(".companyCountry").val());
-	alert($(".companyProvince").val());
-
+$('.hqcompanyCountry').replaceWith('<input class="hqfield hqcompanyCountry"  name="hqcompanyCountry" id="hqcompanyCountry" type="text" />');
+$('.hqcompanyProvince').replaceWith('<input class="hqfield hqcompanyProvince" name="hqcompanyProvince" id="hqcompanyProvince" type="text" />');
 // And copy over the values from the company fields
 	$(".hqaddress1").val($(".address1").val());
 	$(".hqaddress2").val($(".address2").val());
 	$(".hqcompanyCountry").val($(".companyCountry").val());
-	setTimeout(function(){
 	$(".hqcompanyProvince").val($(".companyProvince").val());
-	$(".hqPostalCode").val($(".companyPostalCode").val());},5000);
+	$(".hqPostalCode").val($(".companyPostalCode").val());
+
+
+$(".hqfield").prop('disabled', true);
 }
+
 
 // Otherwise, forget about it...
 else {
 	$(".hqfield").prop('disabled', false);
+  $('.hqcompanyCountry').replaceWith("<select class='hqfield hqcompanyCountry' name='hqcompanyCountry' id='hqcompanyCountry' onchange='print_state(\"hqcompanyProvince\",this.selectedIndex);'><option value='0'>Select Country</option></select>");
+  $('.hqcompanyProvince').replaceWith('<select class="hqfield hqcompanyProvince" name="hqcompanyProvince" id="hqcompanyProvince"><option value="0">Select Province/State</option></select><script language="javascript">print_country("hqcompanyCountry");</script>');
+
 }
 
 });
