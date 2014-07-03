ddb28ad1c2d6177ffc26eb21de5980d784afbf4e
Cael Olsen <colsen@procur.com>
2014-07-03 08:56:43 -0400

    dropdown header styling update

--- assets/linker/js/dropdown-helper.js
+++ assets/linker/js/dropdown-helper.js
@@ -4,7 +4,7 @@
 		if (direction == 'down'){
 			$(".sticky-header").animate({top:["0px","easeOutExpo"]},600);
 		} else {
-			$(".sticky-header").animate({top:["-40px","easeOutExpo"]},600);
+			$(".sticky-header").animate({top:["-54px","easeOutExpo"]},600);
 		}
 		
 	},{ offset: -80});
