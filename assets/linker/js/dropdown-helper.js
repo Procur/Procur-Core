56f5be02a77fb9db8f9160dd8090f89b6a4db93a
Cael Olsen <colsen@procur.com>
2014-07-03 08:51:10 -0400

    update dropdown header full-width and animation

--- assets/linker/js/dropdown-helper.js
+++ assets/linker/js/dropdown-helper.js
@@ -2,9 +2,9 @@
 	$('.container').waypoint(function(direction){
 		console.log('Basic example callback triggered from'+direction);
 		if (direction == 'down'){
-			$(".sticky-header").slideDown(200,'linear');
+			$(".sticky-header").animate({top:["0px","easeOutExpo"]},600);
 		} else {
-			$(".sticky-header").slideUp(200,'linear');
+			$(".sticky-header").animate({top:["-40px","easeOutExpo"]},600);
 		}
 		
 	},{ offset: -80});
