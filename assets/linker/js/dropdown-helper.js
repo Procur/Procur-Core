6d4e03f378fc3a83ce038f53f93ac2772ba3a3aa
Trey Schneider <tschneider@procur.com>
2014-07-03 11:18:37 -0400

    dropper fix

--- assets/linker/js/dropdown-helper.js
+++ assets/linker/js/dropdown-helper.js
@@ -1,6 +1,5 @@
 
 	$('.container').waypoint(function(direction){
-		console.log('Basic example callback triggered from'+direction);
 		if (direction == 'down'){
 			$(".sticky-header").slideDown(200,'linear');
 		} else {
