240d7d39f521024a0822dd43106a86723cf648ab
Trey Schneider <tschneider@procur.com>
2014-07-08 13:53:02 -0400

    remove console.logs

--- assets/linker/js/companyNameExists.js
+++ assets/linker/js/companyNameExists.js
@@ -3,11 +3,9 @@ $('#desiredCompanyName').keyup(function() {
   $.get('/checkCompanyExists', { newCompanyName: newCompanyName }).done(function(data) {
     if (data === true) {
       $('#desiredCompanyNameStatus').html('This company name already exists');
-      console.log('this company name exists bitch!');
     }
     if (data === false) {
-      $('#desiredCompanyNameStatus').html('This company name is up for grabs!!!!');
-      console.log('this is up for grabs!!!');
+      $('#desiredCompanyNameStatus').html('This company name is up for grabs');
     }
   });
 });
