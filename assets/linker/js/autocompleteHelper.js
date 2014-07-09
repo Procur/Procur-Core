79d4f95cbb919864919504c87ab1c9c4eb3ded11
Trey Schneider <tschneider@procur.com>
2014-07-09 10:02:17 -0400

    autocomplete category fix

---
+++ assets/linker/js/autocompleteHelper.js
@@ -0,0 +1,61 @@
+/*$(document).on('click', function(){
+  var contents = ($('#autocomplete1').val());
+  console.log(contents);
+  var n = contents.indexOf(">");
+  console.log(n);
+  if (n != -1){
+    $('.ui-helper-hidden-accessible').css("display","none");
+  }
+});
+*/
+
+
+$('#autocomplete1').on('focus', function(){
+  $('#autocomplete1').on('keyup', function(){
+    var contents = $(this).val().length;
+    if (contents > '2'){
+    $('.autocompleteWrapper1').find('.ui-helper-hidden-accessible').css("display","inline");
+    }
+  });
+  $(document).on('click', function(){
+    var contents = ($('#autocomplete1').val());
+    var n = contents.indexOf('>');
+    var m = contents.indexOf('&');
+
+    if (n != -1 || m != -1){
+      $('.ui-helper-hidden-accessible').css("display","none");
+    }
+  });
+});
+
+$('#autocomplete2').on('focus', function(){
+  $('#autocomplete2').on('keyup', function(){
+    var contents = $(this).val().length;
+    if (contents > '2'){
+    $('.autocompleteWrapper2').find('.ui-helper-hidden-accessible').css("display","inline");
+    }
+  });
+  $(document).on('click', function(){
+    var contents = ($('#autocomplete2').val());
+    var n = contents.indexOf('>','&');
+    if (n != -1){
+      $('.ui-helper-hidden-accessible').css("display","none");
+    }
+  });
+});
+
+$('#autocomplete3').on('focus', function(){
+  $('#autocomplete3').on('keyup', function(){
+    var contents = $(this).val().length;
+    if (contents > '2'){
+    $('.autocompleteWrapper3').find('.ui-helper-hidden-accessible').css("display","inline");
+    }
+  });
+  $(document).on('click', function(){
+    var contents = ($('#autocomplete3').val());
+    var n = contents.indexOf('>','&');
+    if (n != -1){
+      $('.ui-helper-hidden-accessible').css("display","none");
+    }
+  });
+});
