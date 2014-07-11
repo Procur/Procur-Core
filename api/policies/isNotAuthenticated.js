44720a2f40dbd9ae971ea052ccf8b4ff00a205a6
Will Ricketts <will@willricketts.com>
2014-07-11 18:21:47 -0400

    created isNotAuthenticated policy

---
+++ api/policies/isNotAuthenticated.js
@@ -0,0 +1,18 @@
+/**
+ * isNotAuthenticated
+ *
+ * @module      :: Policy
+ * @description :: Simple policy to allow any non-authenticated user
+ *                 Assumes that your login action in one of your controllers sets `req.session.authenticated = true;`
+ * @docs        :: http://sailsjs.org/#!documentation/policies
+ *
+ */
+module.exports = function (req, res, next) {
+  var is_auth = req.session.authenticated;
+  if (is_auth != true) {
+    return next();
+  }
+  else {
+    return res.redirect("/dashboard");
+  };
+};
