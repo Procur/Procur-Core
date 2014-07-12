67c7311c6b299d17d71b2fb7c2d02e2edd83ef64
Trey Schneider <tschneider@procur.com>
2014-07-12 14:58:21 -0400

    update color helper

--- assets/linker/js/changeDropdownColor.js
+++ assets/linker/js/changeDropdownColor.js
@@ -1,7 +1,9 @@
-$('select').change(function(){
-	$(this).css({'color':'#000000','font-weight': 400});
+
+
+$('#regWizard').find('select').change(function(){
+	$(this.css({'color':'#000000','font-weight': 400});
 });
 
-$('input').keyup(function(){
+$('#regWizard').find('input').keyup(function(){
 	$(this).css({'color':'#000000','font-weight': 400});
 });
