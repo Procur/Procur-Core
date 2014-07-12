c8414a91eb2974339f1386196f7d862be4660d0b
Adam Morgan <atom.morgan@gmail.com>
2014-07-12 13:35:50 -0400

    Fix bug to add value attribute with data to pre-populated dropdowns

--- assets/linker/js/countries.js
+++ assets/linker/js/countries.js
@@ -286,7 +286,7 @@ function edit_country(default_value, country_id){
   // given the id of the <select> tag as function argument, it inserts <option> tags
   var option_str = document.getElementById(country_id);
   option_str.length=0;
-  option_str.options[0] = new Option(default_value,'');
+  option_str.options[0] = new Option(default_value, default_value);
   option_str.selectedIndex = 0;
   for (var i=0; i<country_arr.length; i++) {
     option_str.options[option_str.length] = new Option(country_arr[i],country_arr[i]);
