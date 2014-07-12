bde8cd73ee61fe7078521abdfc1d979b841adc3b
Will Ricketts <will@willricketts.com>
2014-07-12 17:21:04 -0400

    removed socketio and commented out respective client side js files

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // Start sails and pass it command line arguments
 var newrelic = require('newrelic');
-require('sails').lift(require('optimist').argv);
+var sails = require('sails');
+var config = sails.util.merge(require('optimist').argv,{
+  hooks: {
+    sockets: false,
+    pubsub: false
+  }
+});
+require('sails').lift(config);
