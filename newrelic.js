f95ff2ad7b23eb72ca31640f3c874be1ca0c7f6b
Will Ricketts <will@willricketts.com>
2014-07-12 17:10:46 -0400

    newrelic configuration change

--- newrelic.js
+++ newrelic.js
@@ -19,6 +19,9 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level : 'warn'
+    level : 'warn',
+    rules: {
+      ignore: ['^/socket.io/*/xhr-polling']
+    }
   }
 };
