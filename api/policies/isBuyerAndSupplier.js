199072360e2b8dc37c1c780dcb84d9b7786f2c0d
Will Ricketts <will@willricketts.com>
2014-07-12 11:22:24 -0400

    added isBuyerAndSupplier policy

---
+++ api/policies/isBuyerAndSupplier.js
@@ -0,0 +1,23 @@
+/**
+ * isBuyerAndSupplier
+ *
+ * @module      :: Policy
+ * @description :: Checks for buyer and supplier on company
+ *
+ *
+ *
+ */
+module.exports = function (req, res, next) {
+  User.findOne({ id: req.session.passport.user }, function(err, user){
+    if(err) return res.redirect('/dashboard', 500);
+    Company.findOne({ user: user.id }, function(err, company){
+      if(err) return res.redirect('/dashboard', 500)
+      if((company.buyer == true)&& (company.supplier == true)) {
+        return next();
+      }
+      else {
+        return res.redirect('/dashboard');
+      }
+    });
+  });
+};
