854d3343e8dca31cef76cde6877e6dc33580e0ab
Trey Schneider <tschneider@procur.com>
2014-07-12 15:14:39 -0400

    font weight change?

--- assets/linker/js/changeDropdownColor.js
+++ assets/linker/js/changeDropdownColor.js
@@ -1,9 +1,10 @@
 
 
 $('#regWizard').find('select').change(function(){
-	$(this.css({'color':'#000000','font-weight': 400});
+	$(this).css({'color':'#000000','font-weight': 400});
 });
 
 $('#regWizard').find('input').keyup(function(){
 	$(this).css({'color':'#000000','font-weight': 400});
+
 });
