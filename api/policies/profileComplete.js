e4401d0fb763ac78ed60b1fbe0a5443aca6bd3fd
Tmoss11 <tmoss>
2014-07-13 18:03:14 -0400

    removed if statement causing redirect for now

--- api/policies/profileComplete.js
+++ api/policies/profileComplete.js
@@ -18,11 +18,13 @@ module.exports = function (req, res, next) {
         if (req.route.path == '/welcome') {
           res.redirect('/dashboard');
         }
+/*
         else if((req.route.path == '/welcome/buyer') || (req.route.path == '/welcome/supplier')){
           if(user.profileComplete == true){
             res.redirect('/dashboard');
           }
-        }
+        }*/
+
         else {
           return next();
         }
