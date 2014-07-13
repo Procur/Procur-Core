076d2da6c0bc1f153f6f27115dea68fde5c7b6c8
Adam Morgan <atom.morgan@gmail.com>
2014-07-12 20:15:42 -0400

    Add update action for Buyer Descriptions

--- api/controllers/BuyerController.js
+++ api/controllers/BuyerController.js
@@ -288,47 +288,31 @@ module.exports = {
   },
 
   updateDescriptions: function (req, res) {
-    var activeUser = req.session.passport.user,
-        p = req.body;
-    User.findOne({ id: activeUser }, function(err, user){
-      if(err){ return res.redirect('/dashboard'); }
-      if(user !== undefined){
-        Company.findOne({ user: user.id }, function(err, company){
-          if(err){ return res.redirect('/dashboard'); }
-          if(company !== undefined){
-            Buyer.findOne({ company: company.id }, function(err, buyer){
-              if(err){ return res.redirect('/dashboard'); }
-              if(buyer !== undefined){
-                Buyer.update(buyer.id, {
-                  companyDescription: p.companyDescription,
-                  environmentalSustainability: p.environmentalSustainability,
-                  qualitySourcing: p.qualitySourcing,
-                  workplaceSafety: p.workplaceSafety,
-                  laborEducationTraining: p.laborEducationTraining,
-                  reinvestment: p.reinvestment
-                }, function(err, buyer){
-                  if(err){ return res.redirect('/company/update#descriptionsSupplier'); }
-                  if(buyer){
-                    return "success";
-                  }
-                  else {
-                    return "failed"
-                  }
-                });
-              }
-              else {
-                return res.redirect('/dashboard');
-              }
-            });
-          }
-          else {
-            return res.redirect('/dashboard');
-          }
+    var user = req.session.passport.user;
+    var b = req.body;
+
+    User.findOne({ id: user }, function(err, user) {
+      if (err) { /* do something here */ }
+      if (user === undefined) { /* do something here */ }
+      Company.findOne({ user: user.id }, function(err, company) {
+        if (err) { /* do something here */ }
+        if (company === undefined) { /* do something here */ }
+        Buyer.findOne({ company: company.id }, function(err, buyer) {
+          if (err) { /* do something here */ }
+          if (buyer === undefined) { /* do something here */ }
+          Buyer.update(buyer.id, {
+            companyDescription: b.companyDescription,
+            environmentalSustainability: b.environmentalSustainability,
+            qualitySourcing: b.qualitySourcing,
+            workplaceSafety: b.workplaceSafety,
+            laborEducationTraining: b.laborEducationTraining,
+            reinvestment: b.reinvestment
+          }).exec(function(err, newBuyer) {
+            if (err) { /* do something here */ }
+            return res.redirect('/company/update#descriptionsBuyer');
+          });
         });
-      }
-      else{
-        return res.redirect('/dashboard');
-      }
+      });
     });
   },
 
