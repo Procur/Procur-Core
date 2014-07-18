7f27b4230f34422404d266a3a3b97c2125534d6f
Trey Schneider <tschneider@procur.com>
2014-07-18 11:24:28 -0400

    remove console log for js helper

--- assets/linker/js/dashboardSubHeaderLogic.js
+++ assets/linker/js/dashboardSubHeaderLogic.js
@@ -3,31 +3,30 @@ function isDashboard(){
 	a.href = '';
 
 	if ( a.href.indexOf('dashboard') != -1){
-
-		$('#dashboard-header.subnav').find('a.dashboard').css({'color':'white', 'font-weight': '300'});
-		$('#dashboard-header.subnav').find('a.dashboard').removeAttr("href");
-		$('#dashboard-header.subnav').find('a.dashboard').addClass("active");
+		$('#dashboard-header').find('a.dashboard').css({'color':'white', 'font-weight': '300'});
+		$('#dashboard-header').find('a.dashboard').removeAttr("href");
+		$('#dashboard-header').find('a.dashboard').addClass("active");
 	} else if ( a.href.indexOf('companies') != -1){
 
-		$('#dashboard-header.subnav').find('a.company').css({'color':'white', 'font-weight': '300'});
-		$('#dashboard-header.subnav').find('a.company').removeAttr("href");
-		$('#dashboard-header.subnav').find('a.company').addClass("active");
+		$('#dashboard-header').find('a.company').css({'color':'white', 'font-weight': '300'});
+		$('#dashboard-header').find('a.company').removeAttr("href");
+		$('#dashboard-header').find('a.company').addClass("active");
 	} else if ( a.href.indexOf('company') != -1){
 
-		$('#dashboard-header.subnav').find('a.updateCompany').css({'color':'white', 'font-weight': '300'});
-		$('#dashboard-header.subnav').find('a.updateCompany').removeAttr("href");
-		$('#dashboard-header.subnav').find('a.updateCompany').addClass("active");
+		$('#dashboard-header').find('a.updateCompany').css({'color':'white', 'font-weight': '300'});
+		$('#dashboard-header').find('a.updateCompany').removeAttr("href");
+		$('#dashboard-header').find('a.updateCompany').addClass("active");
 	}	else if ( a.href.indexOf('user') != -1){
 
-		$('#dashboard-header.subnav').find('a.updateUser').css({'color':'white', 'font-weight': '300'});
-		$('#dashboard-header.subnav').find('a.updateUser').removeAttr("href");
-		$('#dashboard-header.subnav').find('a.updateUser').addClass("active");
+		$('#dashboard-header').find('a.updateUser').css({'color':'white', 'font-weight': '300'});
+		$('#dashboard-header').find('a.updateUser').removeAttr("href");
+		$('#dashboard-header').find('a.updateUser').addClass("active");
 	}
 }
 
 isDashboard();
 
-$('#dashboard-header.subnav').find('a.active').hover(function() {
+$('#dashboard-header').find('a.active').hover(function() {
 	$(this).css({'background': '#333132'});
 });
 /*
--- assets/linker/js/footerStaticMobile.js
+++ assets/linker/js/footerStaticMobile.js
@@ -1,8 +1,9 @@
-var mq = window.matchMedia( "(min-width: 500px)" );
+/*var mq = window.matchMedia( "(min-width: 500px)" );
 if (mq.matches) {
-	console.log('window more than 500px');
+
   $('#dashboard-footer').find('nav').addClass('navbar-fixed-bottom');
 }
 else {
-	console.log('window less than 500px');
+
 }
+*/
