d26e192dc9ffa4cb26054860cd03a4cf829c49a2
Nick Incardone <nickincardone@gmail.com>
2014-07-23 17:01:52 -0400

    removed duplicates in assorted dropdowns, and added cage number validation statements

--- assets/linker/js/formvalidation.js
+++ assets/linker/js/formvalidation.js
@@ -108,6 +108,7 @@ var dnsMsg = "Please enter a 9-digit DNS Number.";
 var otherLocationsMsg = "Include the name, type, & country when providing a location.";
 var agreeTermsMsg = "You must read & agree to all of the above.";
 var responsiblePartyMsg = "You must assert the above.";
+var cageMsg = "Please enter a 5 characters.";
 
 /*
 
@@ -1199,6 +1200,10 @@ $('#supplierInformationForm').validate({//TO-DO: WHY DOESNT THIS WORK??
     }
   },
   messages:{
+    cageCode: {
+      minlength: cageMsg,
+      maxlength: cageMsg
+    },
     acceptedDeliveryTerms: {
       required: checkBoxMsg
     },
