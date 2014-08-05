2d0829d4b865e9755078d0d24e6441a3bf8ac6c2
Adam Morgan <atom.morgan@gmail.com>
2014-08-05 12:15:53 -0400

    Add placeholder image and clean code

--- assets/linker/js/imagePreview.js
+++ assets/linker/js/imagePreview.js
@@ -57,13 +57,44 @@ $(".photo-preview-area").on("click", "i.fa-times", function() {
   }
 });
 
-function showCoords(c) {
-  croppedImageDimensions["x"] = Math.floor(c.x);
-  croppedImageDimensions["y"] = Math.floor(c.y);
-  croppedImageDimensions["x2"] = Math.floor(c.x2);
-  croppedImageDimensions["y2"] = Math.floor(c.y2);
-  croppedImageDimensions["w"] = Math.floor(c.w);
-  croppedImageDimensions["h"] = Math.floor(c.h);
+function readURL(input) {
+  var isValidSize;
+
+  if ( $(".jcrop-holder").length ) { destroyJcrop(); }
+
+  if (input.files && input.files[0]) {
+    file = input.files && input.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      filePath = e.target.result;
+
+      isValidSize = isFileSizeValid(file, e);
+      initDimensions();
+
+      if (isValidSize) {
+        addValidStyling(e);
+        createJcrop();
+        sizeJcrop();
+      } else {
+        addInvalidStyling();
+      }
+
+    }
+      
+    reader.readAsDataURL(input.files[0]); 
+  }
+}
+
+function destroyJcrop() {
+  $("#img-preview").css("visibility", "");
+  $("#img-preview").css("width", "");
+  $("#img-preview").css("height", "");
+  jcrop.destroy();
+}
+
+function isFileSizeValid(file, e) {
+  return file.size > 2000000 ? false : true;
 }
 
 function initDimensions() {
@@ -77,76 +108,66 @@ function initDimensions() {
   };
 }
 
+function addValidStyling(e) {
+  $("#upload-photo").removeClass("disabled");
+  $("#upload-photo").addClass("enabled");
+  $("#upload-photo").removeClass("hide");
+  $("#upload-photo").addClass("show");
+
+  $('#img-preview').attr('src', ' ');
+  $('#img-preview').attr('src', e.target.result);
+  $('#img-preview').css('display','block');
+}
+
+function addInvalidStyling() {
+  $("#upload-photo").removeClass("enabled");
+  $("#upload-photo").addClass("disabled");
+  $("#upload-photo").removeClass("hide");
+  $("#upload-photo").addClass("show");
+
+  $('#img-preview').attr('src', ' ');
+  $('#img-preview').attr('src', '/images/imageTooLarge.png'); 
+  $('#img-preview').css('display','block');
+}
+
+function createJcrop() {
+  $('#img-preview').Jcrop({
+    onSelect: showCoords,
+    onChange: showCoords,
+    bgColor: 'black',
+    bgOpacity: .60,
+    minSize: [50, 50],
+    aspectRatio: 16 / 9,
+    boxWidth: 300,
+    allowMove: true
+  }, function() {
+    jcrop = this;
+  });
+}
+
+function sizeJcrop() {
+  $('.img-container').css({
+    width: '300px'
+  });
+  $('.jcrop-tracker').css({
+    width: '300px'
+  });
+  $('.jcrop-holder').css({
+    width: '300px'
+  });
+}
+
+function showCoords(c) {
+  croppedImageDimensions["x"] = Math.floor(c.x);
+  croppedImageDimensions["y"] = Math.floor(c.y);
+  croppedImageDimensions["x2"] = Math.floor(c.x2);
+  croppedImageDimensions["y2"] = Math.floor(c.y2);
+  croppedImageDimensions["w"] = Math.floor(c.w);
+  croppedImageDimensions["h"] = Math.floor(c.h);
+}
+
 function cropperIsFullImage(dimensions) {
   if (dimensions["w"] == 0 || dimensions["h"] == 0) {
     initDimensions();
-  }
-}
-
-function isFileSizeValid(file, e) {
-  if (file.size > 2000000) {
-    $("#upload-photo").removeClass("enabled");
-    $("#upload-photo").addClass("disabled");
-    $("#upload-photo").removeClass("hide");
-    $("#upload-photo").addClass("show");
-
-    $('#img-preview').attr('src', ' ');
-    $('#img-preview').attr('src', '/images/addUserPhoto.png'); 
-    $('#img-preview').css('display','block');
-  } else {
-    $("#upload-photo").removeClass("disabled");
-    $("#upload-photo").addClass("enabled");
-    $("#upload-photo").removeClass("hide");
-    $("#upload-photo").addClass("show");
-
-    $('#img-preview').attr('src', ' ');
-    $('#img-preview').attr('src', e.target.result);
-    $('#img-preview').css('display','block');
-  }
-}
-
-function readURL(input) {
-  if ( $(".jcrop-holder").length ) {
-    $("#img-preview").css("visibility", "");
-    $("#img-preview").css("width", "");
-    $("#img-preview").css("height", "");
-    jcrop.destroy();
-  }
-
-  if (input.files && input.files[0]) {
-    file = input.files && input.files[0];
-    var reader = new FileReader();
-
-    reader.onload = function (e) {
-      filePath = e.target.result;
-
-      isFileSizeValid(file, e);
-      initDimensions();
-
-      $('#img-preview').Jcrop({
-        onSelect: showCoords,
-        onChange: showCoords,
-        bgColor: 'black',
-        bgOpacity: .60,
-        minSize: [50, 50],
-        aspectRatio: 16 / 9,
-        boxWidth: 300,
-        allowMove: true
-      }, function() {
-        jcrop = this;
-      });
-
-      $('.img-container').css({
-        width: '300px'
-      });
-      $('.jcrop-tracker').css({
-        width: '300px'
-      });
-      $('.jcrop-holder').css({
-        width: '300px'
-      });
-    }
-      
-    reader.readAsDataURL(input.files[0]); 
   }
 }
